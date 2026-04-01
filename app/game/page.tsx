1f36db91abc6eef82b6d4166574b1b6ed07a8b8a
ayushagarwalhere <ayush.agarwal.ayush.21@gmail.com>
2026-04-01 08:18:51 +0530

    .

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -16,7 +16,7 @@ import {
 import { parseMarkdown } from "./parseMarkdown"
 
 const GAME_START = new Date(
-  process.env.NEXT_PUBLIC_GAME_START ?? "2026-03-01T22:00:00+05:30"
+  process.env.NEXT_PUBLIC_GAME_START ?? "2026-04-01T22:00:00+05:30"
 )
 
 export default function GamePage() {
